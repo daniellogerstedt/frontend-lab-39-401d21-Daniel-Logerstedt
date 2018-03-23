61c13d4a811e606c9cc149ea57f05803c6ac73e2
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-03-22 17:02:24 -0700

    added index.js to fix heroku deploy

--- src/reducer/auth.js
+++ src/reducer/auth.js
@@ -1,12 +1,12 @@
 export default (state=null, action) => {
-  let {type, payload} = action
+  let {type, payload} = action;
 
   switch(type) {
-  case 'TOKEN_SET': return payload
+  case 'TOKEN_SET': return payload;
   case 'TOKEN_DELETE': {
     delete localStorage.token;
     return null;
   }
-  default: return state
+  default: return state;
   }
-}
+};
